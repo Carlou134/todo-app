f5d68bb7666ebcca0db14ec0454245742d459d94
Carlou134 <carlouvasquez134@gmail.com>
2025-10-27 23:05:16 -0500

    Creación del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-app';
+  welcome = 'Hola!';
+  tasks = [
+    'Instalar el Angular CLI',
+    'Crear proyecto',
+    'Crear componente'
+  ]
 }

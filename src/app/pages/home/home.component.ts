9ad1fb432e6008b697f469b9b0312e4303b7e5fc
Carlou134 <carlouvasquez134@gmail.com>
2025-10-31 02:18:15 -0500

    Gestión dinámica

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { NgFor } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Tasks } from '../../models/task.model';
 
 @Component({
   selector: 'app-home',
@@ -9,16 +10,32 @@ import { Component, signal } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  tasks = signal([
-      'Instalar el Angular CLI',
-      'Crear proyecto',
-      'Crear componente'
+  tasks = signal<Tasks[]>([
+    {
+      id: Date.now(),
+      title: "Crear Proyecto",
+      completed: false
+    },
+    {
+      id: Date.now(),
+      title: "Crear componente",
+      completed: false
+    },
     ]);
 
     changeHandler(event: Event){
       const input = event.target as HTMLInputElement;
-      const newTasks = input.value;
-      this.tasks.update((tasks) => [...tasks, newTasks]);
+      const newTask = input.value;
+      this.addTask(newTask);
+    }
+
+    addTask(title: string){
+      const newTask = {
+        id: Date.now(),
+        title: title,
+        completed: false
+      }
+      this.tasks.update((tasks) => [...tasks, newTask]);
     }
 
     deleteTask(index: number){
